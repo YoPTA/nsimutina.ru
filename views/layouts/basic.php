cf56e64c078bc410d8a7a3892757320a4a228264
Sergey Nichipurenko <s.nichipurenko@yandex.ru>
2015-11-05 19:48:01 +0300

    Добавление стилей и js

--- views/layouts/basic.php
+++ views/layouts/basic.php
@@ -1,20 +1,29 @@
 <?php
+use app\assets\AppAsset;
 /*
- *
+ * @var $content string
+ * @var $this \yii\web\View
  */
-/* @var $content string */
+AppAsset::register($this); // Регистрируем AppAsset
+$this->beginPage(); // Метка начала документа
 ?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
     <title><?= Yii::$app->name ?></title>
+    <?php $this->head(); ?>
 </head>
 <body>
+<?php $this->beginBody(); ?>
     <p>Верхняя часть сайта</p>
 
     <?= $content; ?>
 
     <p>Нижняя часть сайта</p>
+<?php $this->endBody(); ?>
 </body>
 </html>
+<?php
+    $this->endPage(); // Метка конца страницы
+?>
